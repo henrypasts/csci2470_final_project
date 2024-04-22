41bb381ea0c3f92eb7adc44bda1eaa15fc432880
mangelakis <85129012+mangelakis@users.noreply.github.com>
2024-04-21 22:45:25 -0400

    using cld3 instead of langid

--- sentiment/preprocess.py
+++ sentiment/preprocess.py
@@ -2,20 +2,19 @@ import pandas as pd
 from langdetect import detect
 from langid import classify     # faster than langdetect
 from tqdm import tqdm
+import cld3
 
 PROCESS_ALL = True
-
 TWEETS_CSV = 'data/tweets.csv'
 TWEETS_CSV_OUT = 'data/cleaned_tweets.csv' if PROCESS_ALL else 'data/cleaned_tweets_sample.csv'
 
-
 def detect_language(text):
     """
-    Detects language of text
+    Detects language of text using cld3
     """
     try:
         if bool(text.strip()) and len(text.strip()) >= 10:
-            return classify(text)[0]
+            return cld3.get_language(text).language
         else:
             return "invalid"
     except:
@@ -26,21 +25,20 @@ def clean_data():
     Cleans data by removing non-english tweets
     """
     print("Running clean data...")
-    df = pd.read_csv(TWEETS_CSV, 
-                    sep=';', 
-                    nrows=None if PROCESS_ALL else 10000,
-                    usecols=['timestamp','text'],         
-                    dtype={'timestamp': 'str', 'text': 'str'}
-                    )
+    df = pd.read_csv(TWEETS_CSV,
+                     sep=';',
+                     nrows=None if PROCESS_ALL else 10000,
+                     usecols=['timestamp', 'text'],
+                     dtype={'timestamp': 'str', 'text': 'str'})
+
     tqdm.pandas()
     df['lang'] = df['text'].progress_apply(detect_language)
     print(df['lang'].value_counts())
+
     df = df[df['lang'] == 'en']
-    df.to_csv(TWEETS_CSV_OUT, index=False, sep=';')     # Save cleaned data
+    df.to_csv(TWEETS_CSV_OUT, index=False, sep=';')  # Save cleaned data
+
     return df
-
-
-
 
 if __name__ == '__main__':
     clean_data()
