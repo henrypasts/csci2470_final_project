3b214747ffafd03d0d101b474e2cf64c5fce2a36
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2024-04-24 15:05:11 -0400

    chunking

--- sentiment/preprocess.py
+++ sentiment/preprocess.py
@@ -1,10 +1,10 @@
 import pandas as pd
+from tqdm import tqdm
 from langdetect import detect
 from langid import classify     # faster than langdetect
-from tqdm import tqdm
-import cld3
+# import cld3                   # faster than langid
 
-PROCESS_ALL = True
+PROCESS_ALL = False
 TWEETS_CSV = 'data/tweets.csv'
 TWEETS_CSV_OUT = 'data/cleaned_tweets.csv' if PROCESS_ALL else 'data/cleaned_tweets_sample.csv'
 
@@ -14,6 +14,8 @@ def detect_language(text):
     """
     try:
         if bool(text.strip()) and len(text.strip()) >= 10:
+            # return detect(text)
+            return classify(text)[0]
             return cld3.get_language(text).language
         else:
             return "invalid"
@@ -25,15 +27,25 @@ def clean_data():
     Cleans data by removing non-english tweets
     """
     print("Running clean data...")
-    df = pd.read_csv(TWEETS_CSV,
+    chunks = pd.read_csv(TWEETS_CSV,
                      sep=';',
                      nrows=None if PROCESS_ALL else 10000,
                      usecols=['timestamp', 'text'],
-                     dtype={'timestamp': 'str', 'text': 'str'})
+                     dtype={'timestamp': 'str', 'text': 'str'},
+                     chunksize=1000)
 
+    # tqdm.pandas()
+    # df['lang'] = df['text'].progress_apply(detect_language)
+    # print(df['lang'].value_counts())
+
+    filtered_chunks = []
     tqdm.pandas()
-    df['lang'] = df['text'].progress_apply(detect_language)
-    print(df['lang'].value_counts())
+
+    for chunk in chunks:
+        chunk['lang'] = chunk['text'].progress_apply(detect_language)
+        filtered_chunks.append(chunk)
+
+    df = pd.concat(filtered_chunks)
 
     df = df[df['lang'] == 'en']
     df.to_csv(TWEETS_CSV_OUT, index=False, sep=';')  # Save cleaned data
